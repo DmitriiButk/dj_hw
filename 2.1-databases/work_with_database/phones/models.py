04e43205abc4a126a3b8f65d79648f34df73d81e
DmitriiBut <nudumka21@gmail.com>
2023-10-24 16:41:41 +0200

    Создание моделей базы данных.

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,12 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField()
+    price = models.CharField()
+    image = models.URLField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
