24356ea14ad203ed7dad6b218e87e95b37783a20
DmitriiBut <nudumka21@gmail.com>
2023-10-24 16:43:49 +0200

    Доработка кода функций show_catalog и show_product.

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from phones.models import Phone
 
 
 def index(request):
@@ -6,12 +7,21 @@ def index(request):
 
 
 def show_catalog(request):
+    phones_list = Phone.objects.all()
     template = 'catalog.html'
-    context = {}
+    sort_phone = request.GET.get('sort')
+    if sort_phone == 'name':
+        phones_list = Phone.objects.all().order_by('name')
+    elif sort_phone == 'min_price':
+        phones_list = Phone.objects.all().order_by('price')
+    elif sort_phone == 'max_price':
+        phones_list = Phone.objects.all().order_by('-price')
+    context = {'phones': phones_list}
     return render(request, template, context)
 
 
 def show_product(request, slug):
+    phone = Phone.objects.get(slug=slug)
     template = 'product.html'
-    context = {}
+    context = {'phone': phone}
     return render(request, template, context)
