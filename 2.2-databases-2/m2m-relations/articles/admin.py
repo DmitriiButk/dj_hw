9081cb39edb8d92a5dd4171e6b5dea0e73bdbddf
DmitriiBut <nudumka21@gmail.com>
2023-11-07 12:51:47 +0200

    creating and setting up an admin panel

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,32 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Tag, Scope
+
+
+class RelationshipInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        count = 0
+        for form in self.forms:
+            if form.cleaned_data.get('is_main'):
+                count += 1
+            if count > 1:
+                raise ValidationError('Основной тэг уже выбран.')
+        return super().clean()
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+class ScopeInLine(admin.TabularInline):
+    model = Scope
+    formset = RelationshipInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'title', 'published_at', 'image']
+    inlines = [ScopeInLine, ]
