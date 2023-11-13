63bfb802ee3cb8e75a0d392d37e7b12df42ee97b
DmitriiBut <nudumka21@gmail.com>
2023-11-13 16:31:24 +0200

    add views functions post and patch

--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,31 @@
-# TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
-# TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from .models import Measurement, Sensor
+from .serializers import SensorDetailSerializer, MeasurementSerializer, SensorSerializer, \
+    SensorUpdateSerializer, MeasurementUpdateSerializer
+from rest_framework.response import Response
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView, RetrieveAPIView
+
+
+class SensorsViewAndCreate(ListCreateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+    def post(self, request):
+        serializer = SensorSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+
+class MeasurementUpdateView(ListCreateAPIView):
+    serializer_class = MeasurementUpdateSerializer
+
+
+class SensorViewAndUpdate(RetrieveUpdateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
+
+    def patch(self, request, *args, **kwargs):
+        serializer = SensorDetailSerializer(data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return super().patch(request)
