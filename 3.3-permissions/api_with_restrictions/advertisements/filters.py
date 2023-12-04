7be6a049ecc820706c65ace43256f9b4582997b7
DmitriiButk <nudumka21@gmail.com>
2023-12-04 15:09:15 +0200

    add filters

--- 3.3-permissions/api_with_restrictions/advertisements/filters.py
+++ 3.3-permissions/api_with_restrictions/advertisements/filters.py
@@ -1,12 +1,14 @@
 from django_filters import rest_framework as filters
 
-from advertisements.models import Advertisement
+from .models import Advertisement
 
 
 class AdvertisementFilter(filters.FilterSet):
     """Фильтры для объявлений."""
-
-    # TODO: задайте требуемые фильтры
+    status = filters.CharFilter(field_name='status')
+    created_at_before = filters.DateFilter(field_name='created_at', lookup_expr='lte')
+    created_at_after = filters.DateFilter(field_name='created_at', lookup_expr='gte')
 
     class Meta:
         model = Advertisement
+        fields = ['created_at', 'status', 'creator']
